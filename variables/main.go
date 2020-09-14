8df19879dc9fd1a9af164a4fe172d0c21178de17
JulioConchas <conchasjimenez@gmail.com>
2020-09-14 13:18:50 -0500

    Act2: func area_cuadrado agregada

--- variables/main.go
+++ variables/main.go
@@ -8,7 +8,7 @@ func main(){
     fmt.Scanf("%d",&option)
 
     switch option {
-    case 1: fmt.Println("Area del cuadrado")
+    case 1: area_cuadrado()
     case 2: fmt.Println("Area triangulo")
     case 3: fmt.Println("Area circulo")
     case 4: fmt.Println("Fahrenheit to Celcius")
@@ -24,4 +24,13 @@ func menu(){
     fmt.Println("[2] Area del triángulo")
     fmt.Println("[3] Area del círculo")
     fmt.Println("[4] Fahrenheit a Celcius")
+}
+func area_cuadrado(){
+    var lado,resultado float32
+
+    fmt.Println("===== Area del Cuadrado ====")
+    fmt.Println("Ingresa el lado: ")
+    fmt.Scanf("%g",&lado)
+    resultado = lado * lado
+    fmt.Println("Area = ",resultado,"cm")
 }
