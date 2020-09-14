4cee349ac22911dc1c0265a974035cbe302429fd
JulioConchas <conchasjimenez@gmail.com>
2020-09-14 12:10:44 -0500

    Campura de nombre completo usando fgets()

--- main.c
+++ main.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
 int main(){
-	printf("Hello World\n");
+	char nombre[100];
 
+	printf("Hello World\n");
+	printf("Nombre: \n");
+	fgets(nombre,sizeof(nombre),stdin);
+	printf("Hola %s\n",nombre);
 	return 0;
 }
