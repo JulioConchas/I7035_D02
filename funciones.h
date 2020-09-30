5499a9baddba2fe22f019327d8e0af8e1da4dcd6
JulioConchas <conchasjimenez@gmail.com>
2020-09-30 13:15:23 -0500

    Agregada la condificion en resta

--- funciones.h
+++ funciones.h
@@ -2,5 +2,9 @@
 #define FUNCIONES_H
 
 int suma(int a,int b){ return a + b; }
-int resta(int a,int b){ return a - b; }
+int resta(int a,int b){ 
+	if ( b > 0 ){
+		return 0;
+	}
+	return a - b; }
 #endif
