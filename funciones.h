8d6837369e4b7612f0a63c2ba9f56e936a7c03e5
JulioConchas <conchasjimenez@gmail.com>
2020-09-30 13:13:49 -0500

    Modificación en la funcion de resta, condición

--- funciones.h
+++ funciones.h
@@ -2,5 +2,10 @@
 #define FUNCIONES_H
 
 int suma(int a,int b){ return a + b; }
-int resta(int a,int b){ return a - b; }
+int resta(int a,int b){ 
+	if ( a < 0 ){ 
+		return 0;
+	}		
+	return a - b; }
+
 #endif
