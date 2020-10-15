bb578cc9dad1ba702ea5396b07aff0864fd2d574
jconchas <jconchas@ibm.com>
2020-10-15 13:14:49 -0500

    GO (V): Funcio burbujar, works well

--- Slices/main.go
+++ Slices/main.go
@@ -9,9 +9,17 @@ import "fmt"
 func main()  {
     s := []int64{1, -10, 90, 14, -100, -2}
     Burbuja(s)
+    fmt.Println("main: ",s)
 }
 func Burbuja(s []int64){
-    for i := 0;i < len(s);i++ {
-        fmt.Println(s[i])
+    var aux int64
+    for i := 0;i < len(s) ;i++ {
+        for j := 0;j < (len(s) - i - 1);j++ {
+            if s[j] > s[j+1] {
+                aux = s[j]
+                s[j] = s[j+1]
+                s[j+1] = aux
+            }
+        }
     }
 }
