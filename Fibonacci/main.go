209c68252a5295bfcb0896e88495a0c1cd553cb4
jconchas <jconchas@ibm.com>
2020-10-15 16:15:27 -0500

    GO (IV): Variadic works fine

--- Fibonacci/main.go
+++ Fibonacci/main.go
@@ -1,6 +1,7 @@
 package main
 
 import "fmt"
+import "math/rand"
 
 // julio conchas
 // conchasjimenez@gmail.com
@@ -13,6 +14,7 @@ func main()  {
 
     switch option {
     case 1: fubonacci()
+    case 2: variadic()
     default: fmt.Println("Practica número:",option," no existe")
     }
 }
@@ -21,6 +23,35 @@ func menu(){
     fmt.Println("= conchasjimenez@gmail.com =")
     fmt.Println("========= Practicas ========")
     fmt.Println("[1] Fibonacci.")
+    fmt.Println("[2] Variadic Function")
+}
+func variadic(){
+    var slice []int
+
+    for i := 0; i < 100;i++ {
+        slice = append(slice,rand.Intn(50))
+    }
+    // fmt.Println(slice)
+    // fmt.Println("==========================")
+    fmt.Println(bigest_number(slice...))
+}
+func bigest_number(n ...int) int{
+    var b int
+    var flag = true
+    b = 0
+    for i := 0;i < len(n) ;i++ {
+        for j := 0; j < len(n); j++ {
+            if n[i] < n[j] {
+                flag = false
+            }
+        }
+        if flag {
+            b = n[i]
+        } else {
+            flag = true
+        }
+    }
+    return b
 }
 func fubonacci(){
     var x int
