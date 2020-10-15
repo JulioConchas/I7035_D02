15149e74561d1e1a860a334830e6c4564bf89ab4
jconchas <jconchas@ibm.com>
2020-10-15 12:55:25 -0500

    GO (V): creacion de proyecto burbuja test

---
+++ Slices/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+// julio conchas
+// conchasjimenez@gmail.com
+// test_burbuja
+
+func main()  {
+    s := []int64{1, -10, 90, 14, -100, -2}
+    for i := 0;i < len(s);i++ {
+        fmt.Println(s[i])
+    }
+	//fmt.Println(s)
+}
