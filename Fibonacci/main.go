2bdeb83a78b36adf81f3c162266ad0cb8607e3e3
jconchas <jconchas@ibm.com>
2020-10-15 13:23:04 -0500

    GO (V): Estructura de proyecto personal & funcion fibonacci

--- Fibonacci/main.go
+++ Fibonacci/main.go
@@ -7,5 +7,19 @@ import "fmt"
 // Fibonacci
 
 func main()  {
+    var option int
+    menu()
+    fmt.Scanf("%d",&option)
 
+    switch option {
+    case 1: fibonacci()
+    default: fmt.Println("Practica número:",option," no existe")
+    }
+}
+func menu(){
+    fmt.Println("======= Julio Conchas ======")
+    fmt.Println("= conchasjimenez@gmail.com =")
+    fmt.Println("========= Practicas ========")
+    fmt.Println("[1] Fibonacci.")
+    fmt.Println("[2] Número e.")
 }
