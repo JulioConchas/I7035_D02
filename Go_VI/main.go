25b2516809d85611ad3b37c6bb5964674b18726c
jconchas <jconchas@ibm.com>
2020-10-27 10:03:24 -0600

    GO VI: creacion de estructuras y metodos

--- Go_VI/main.go
+++ Go_VI/main.go
@@ -1,15 +1,39 @@
+package main
 
+import "fmt"
 // Author: Julio Conchas
 // Main  : conchasjimenez@gmail.com
 
 type Imagen struct{
-
+    titulo string
+    formato string
+    canales string
 }
 type Audio struct{
-
+    titulo string
+    formato string
+    duracion int
 }
 type Video struct{
-    
+    titulo string
+    formato string
+    frames int
+}
+// methodos
+func (i *Imagen) mostrar {
+    fmt.Println(i.titulo)
+    fmt.Println(i.formato)
+    fmt.Println(i.canales)
+}
+func (a *Audio) mostrar {
+    fmt.Println(a.titulo)
+    fmt.Println(a.formato)
+    fmt.Println(i.duracion)
+}
+func (v *Video) mostrar {
+    fmt.Println(v.titulo)
+    fmt.Println(v.formato)
+    fmt.Println(v.frames)
 }
 func main(){
 
