1a34904604146d08d3f7cd088d0ed8a16ceef498
jconchas <jconchas@ibm.com>
2020-10-29 11:51:17 -0600

    GO_VII: creación de estructuras y metodos sort

--- GO_VII/main.go
+++ GO_VII/main.go
@@ -1,6 +1,11 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "os"
+    "strings"
+    "sort"
+)
 
 // Author: Julio Conchas
 // Main  : conchasjimenez@gmail.com
@@ -8,6 +13,20 @@ import "fmt"
 func main(){
     s := []string{}
     var aux string
+
+    file_ascendente,err := os.Create("ascendente.txt")
+    if err != nil{
+        fmt.Println(err)
+        return
+    }
+    defer file_ascendente.Close()
+
+    file_descendente,err := os.Create("descendente.txt")
+    if err != nil{
+        fmt.Println(err)
+        return
+    }
+    defer file_descendente.Close()
 
     fmt.Println("Ingresa el String..")
     fmt.Scanln(&aux)
@@ -23,5 +42,18 @@ func main(){
             break
         }
     }
-    fmt.Println(s)
+    sort.Sort(ByUpward(s))
+    file_ascendente.WriteString(strings.Join(s,"\n"))
+    sort.Sort(ByFalling(s))
+    file_descendente.WriteString(strings.Join(s,"\n"))
 }
+
+type ByUpward []string
+func (a ByUpward) Len() int { return len(a) }
+func (a ByUpward) Swap(i,j int) { a[i], a[j] = a[j], a[i] }
+func (a ByUpward) Less(i,j int) bool { return a[i] < a[j] }
+
+type ByFalling []string
+func (a ByFalling) Len() int { return len(a) }
+func (a ByFalling) Swap(i,j int) { a[i], a[j] = a[j], a[i] }
+func (a ByFalling) Less(i,j int) bool { return a[i] > a[j] }
