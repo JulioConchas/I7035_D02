6f7f2d99f007085f41a0badbb258744700bac424
jconchas <jconchas@ibm.com>
2020-10-29 11:09:31 -0600

    GO_VII: creación de slice de n strings

--- GO_VII/main.go
+++ GO_VII/main.go
@@ -6,5 +6,22 @@ import "fmt"
 // Main  : conchasjimenez@gmail.com
 
 func main(){
-    
+    s := []string{}
+    var aux string
+
+    fmt.Println("Ingresa el String..")
+    fmt.Scanln(&aux)
+    s = append(s,aux)
+    for true {
+        fmt.Println("Otro string? [y/n]")
+        fmt.Scanln(&aux)
+        if aux=="y"||aux=="Y" {
+            fmt.Println("Ingresele pues...")
+            fmt.Scanln(&aux)
+            s = append(s,aux)
+        } else {
+            break
+        }
+    }
+    fmt.Println(s)
 }
